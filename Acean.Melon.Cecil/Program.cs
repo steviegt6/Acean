c5b3afc2214e9dac6751a4a46b62486bad883e20
Steviegt6 <xxlennygamerxx@gmail.com>
2021-11-03 19:17:09 -0700

    why

---
+++ Acean.Melon.Cecil/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Mono.Collections.Generic;
+using MonoMod.Cil;
+
+namespace Acean.Melon.Cecil
+{
+    public static class Program
+    {
+        public const string AsmGen = "Il2CppAssemblyGenerator.dll";
+
+        public static void Main()
+        {
+            if (!File.Exists(AsmGen))
+                throw new Exception("DLL not found.");
+
+            Console.WriteLine("Found generator DLL.");
+
+            AssemblyDefinition asm = AssemblyDefinition.ReadAssembly(AsmGen);
+
+            TypeDefinition melon = asm.MainModule.GetType(
+                "MelonLoader.Il2CppAssemblyGenerator.RemoteAPI"
+            ).GetNestedType("DefaultHostInfo").GetNestedType("Melon");
+
+            if (melon is null)
+                throw new Exception("Failed to resolve Melon type.");
+
+            Console.WriteLine("Found Melon type.");
+
+            MethodDefinition contact = melon.Methods.Single(x => x.Name == "Contact");
+
+            if (contact is null)
+                throw new Exception("Failed to resolve Contact method.");
+
+            Console.WriteLine("Found Contact method.");
+
+            Console.WriteLine("Rewriting Contact method.");
+
+            ILContext il = new ILContext(contact);
+            ILCursor c = new ILCursor(il);
+
+            void Match() => c.GotoNext(x => x.OpCode == OpCodes.Ldloc_0);
+            void Remove(int range) => c.RemoveRange(range);
+            // void Null() => c.Emit(OpCodes.Ldnull);
+            void String(string str) => c.Emit(OpCodes.Ldstr, str);
+
+            Match();
+            Match();
+            Remove(2);
+            String(""); // Null();
+            Match();
+            Remove(2);
+            String(""); // Null();
+
+            Console.WriteLine("Rewrote Contact method.");
+            Console.WriteLine("Creating new assembly file.");
+
+            asm.Write("ACEAN-" + AsmGen);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(true);
+        }
+
+        public static TypeDefinition GetNestedType(this TypeDefinition self, string fullname)
+        {
+            if (!self.HasNestedTypes)
+                return null;
+
+            Collection<TypeDefinition> nestedTypes = self.NestedTypes;
+
+            return nestedTypes.FirstOrDefault(nestedType => nestedType.TypeFullName() == fullname);
+        }
+
+        public static string TypeFullName(this TypeReference self) =>
+            string.IsNullOrEmpty(self.Namespace)
+                ? self.Name
+                : self.Namespace + '.' + self.Name;
+
+        /*
+         *         public static object Contact(string response_str)
+        {
+            Console.WriteLine("[Acean] Executing modified Contact.");
+
+            object info = orig_Contact(response_str);
+
+            Assembly icagAsm = AppDomain.CurrentDomain.GetAssemblies().Single(
+                x => x.GetName().Name == "Il2CppAssemblyGenerator"
+            );
+
+            Type infoStruct = icagAsm.GetType(
+                "MelonLoader.Il2CppAssemblyGenerator.RemoteAPI.DefaultHostInfo.ResponseStruct"
+            );
+
+            void QuickSet(string field, object value) => infoStruct.GetField(
+                field, BindingFlags.NonPublic | BindingFlags.Instance
+            )?.SetValue(info, value);
+
+            QuickSet("ForceDumperVersion", null);
+            QuickSet("ForceUnhollowerVersion", null);
+
+            Console.WriteLine("[Acean] Executed modified Contact.");
+
+            return info;
+        }
+         */
+    }
+}
---
+++ Acean/AceanMod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Acean;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(AceanMod), "Acean", "0.1.0", "Tomat")]
+[assembly: MelonGame("Dani", "Crab Game")]
+
+namespace Acean
+{
+    public class AceanMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+
+            MelonLogger.Msg(ConsoleColor.Yellow, "Application started.");
+        }
+    }
+}
